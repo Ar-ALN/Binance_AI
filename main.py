d17bdb7453004d9693bb7fbb14428807275aa65b
admin <alanramam@gmail.com>
2021-07-11 22:30:04 +0200

    Reshape fixed price input fixed

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
 import matplotlib.pyplot as plt
 import numpy as np
+
 
 plt.close("all")
 
@@ -12,8 +14,10 @@ data = data.drop(columns=[11])
 
 values = data.values
 
+scaler=MinMaxScaler(feature_range=(0,1))
+
 #met tout entre 0 et 1
-values = (values - values.min(0)) / (values.max(0) - values.min(0))
+values = scaler.fit_transform(np.array(values).reshape(-1,1))
 
 # ts = pd.Series(values[:, 1])
 # plt.figure()
@@ -101,32 +105,45 @@ model.add(Dense(units=1))
 # save_weights_only=False)]
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train,validation_data=(x_test,y_test),
-                    batch_size=2048, epochs=150,
+                    batch_size=64, epochs=1, verbose = 1
 
                     )
 
-# Test data set
-test_data = scaled_data[training_dataset_length - 10:, :]
+### Lets Do the prediction and check performance metrics
+train_predict=model.predict(x_train)
+test_predict=model.predict(x_test)
 
-# splitting the x_test and y_test data sets
-x_test = []
-y_test = features[training_dataset_length:, :]
+### Calculate RMSE performance metrics
+import math
+from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
-for i in range(10, len(test_data)):
-    x_test.append(test_data[i - 10:i, 0])
+math.sqrt(mean_squared_error(y_train,train_predict))
 
-# Convert x_test to a numpy array
-x_test = np.array(x_test)
 
-# Reshape the data into 3-D array
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-# check predicted values
-predictions = model.predict(x_test)
-# Undo scaling
-predictions = scaler.inverse_transform(predictions)
 
-# Calculate RMSE score
-rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
-rmse
+##Transformback to original form
+train_predict=scaler.inverse_transform(train_predict)
+test_predict=scaler.inverse_transform(test_predict)
 
+
+### Test Data RMSE
+math.sqrt(mean_squared_error(ytest,test_predict))
+
+### Plotting
+# shift train predictions for plotting
+look_back=100
+trainPredictPlot = numpy.empty_like(test_data)
+trainPredictPlot[:, :] = np.nan
+trainPredictPlot[look_back:len(train_predict)+look_back, :] = train_predict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(test_data)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(train_predict)+(look_back*2)+1:len(test_data)-1, :] = test_predict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(test_data))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
