ded786eb8720c51eb69917b21fe71f2eaec2837d
Ortes <malo.allee@gmail.com>
2021-07-19 03:46:04 +0200

    Fix coding style + unused code

--- main.py
+++ main.py
@@ -37,35 +37,6 @@ train_data = train_data[0:training_dataset_length, :]
 train_data = train_data[0:]
 test_data = test_data[0:]
 
-# Splitting the data
-# x_train = []
-# y_train = []
-
-# for i in range(10, len(train_data)):
-# x_train.append(train_data[i - 10:i, 0])
-# y_train.append(train_data[i, 0])
-
-# Convert to numpy arrays
-# x_train, y_train = np.array(x_train), np.array(y_train)
-
-# Reshape the data into 3-D array
-# x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-# convert an array of values into a dataset matrix
-def create_dataset(dataset, time_step=1):
-    dataX, dataY = [], []
-    for i in range(len(dataset) - time_step - 1):
-        a = dataset[i:(i + time_step), 0]
-        dataX.append(a)
-        dataY.append(dataset[i + time_step, 0])
-    return np.array(dataX), np.array(dataY)
-
-
-# reshape into X=t,t+1,t+2,t+3 and Y=t+4
-time_step = 100
-x_train, y_train = create_dataset(train_data, time_step)
-x_test, y_test = create_dataset(test_data, time_step)
-
 # Now perform exponential moving average smoothing
 # So the data will have a smoother curve than the original ragged data
 EMA = 0.0
@@ -77,14 +48,18 @@ for ti in range(11000):
 # Used for visualization and test purposes
 all_mid_data = np.concatenate([train_data, test_data], axis=0)
 
-# reshape le tout
-x_train =x_train.reshape(x_train.shape[0],x_train.shape[1] , 1)
-x_test = x_test.reshape(x_test.shape[0],x_test.shape[1] , 1)
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
 
-def deep_network_LSTM(name_model,x_train, y_train, activation_function = 'sigmoid', opt = 'adam', epochs = 100, batch_size = 64):
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, time_step=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset) - time_step - 1):
+        a = dataset[i:(i + time_step), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + time_step, 0])
+    return np.array(dataX), np.array(dataY)
+
+
+def deep_network_LSTM(name_model,x_train, y_train, x_test, y_test, activation_function = 'sigmoid', opt = 'adam', epochs = 100, batch_size = 64):
     # Initialising the RNN
     model = Sequential()
 
@@ -112,16 +87,16 @@ def deep_network_LSTM(name_model,x_train, y_train, activation_function = 'sigmoi
     # ModelCheckpoint('../models/model.h5'), save_best_only=True,
     # save_weights_only=False)]
     model.compile(optimizer= opt, loss='mean_squared_error',metrics=['accuracy'])
-    history =model.fit(x_train, y_train,validation_data=(x_test,y_test),
+    history = model.fit(x_train, y_train,validation_data=(x_test,y_test),
                         batch_size=batch_size, epochs=epochs, verbose = 1
 
                         )
     model.save(name_model)
     print("Saved model to disk")
 
-    return(history)
+    return(model,history)
 
-def prediction_model_plot(model,x_train,x_test, values):
+def prediction_model_plot(model,x_train,y_train,x_test,y_test, values,look_back,x_min=39000,x_max=39500,y_min=32000,y_max=37000):
     ### Lets Do the prediction and check performance metrics
     train_predict = model.predict(x_train)
     test_predict = model.predict(x_test)
@@ -135,7 +110,7 @@ def prediction_model_plot(model,x_train,x_test, values):
 
     ### Plotting
     # shift train predictions for plotting
-    look_back = 100
+
     trainPredictPlot = np.empty_like(values)
     trainPredictPlot[:, :] = np.nan
     trainPredictPlot[look_back:len(train_predict) + look_back, :] = train_predict
@@ -147,6 +122,7 @@ def prediction_model_plot(model,x_train,x_test, values):
     plt.plot(scaler.inverse_transform(values))
     plt.plot(trainPredictPlot)
     plt.plot(testPredictPlot)
+    plt.axis([x_min, x_max, y_min, y_max])
     plt.show()
 
 def plot_hp(model_plot,hyperparameter):
@@ -157,16 +133,61 @@ def plot_hp(model_plot,hyperparameter):
     plt.figure(figsize=(8, 6))
     plt.plot(model.history[train_hp], label='train')
     plt.plot(model.history[validation_hp], alpha=0.7, label='validation')
-    plt.xlabel('Epochs')
+    plt.xlabel(hyperparameter)
     plt.ylabel('Loss')
     plt.legend()
-    plt.title('Loss vs Epochs for Model 1', size=25)
+    plt.title(hyperparameter + ' vs Epochs for Model 1', size=25)
     plt.grid()
+    plt.show()
 
-model1 = deep_network_LSTM('model1',x_train, y_train,epochs=1)
-plot_hp(model1,'loss')
-plot_hp(model1,'accuracy')
-prediction1 = prediction_model_plot(model1,x_train,x_test, values)
+def reshape_data(time_step,train_data,test_data):
+
+    # reshape into X=t,t+1,t+2,t+3 and Y=t+4
+    x_train, y_train = create_dataset(train_data, time_step)
+    x_test, y_test = create_dataset(test_data, time_step)
+
+    # reshape le tout
+    x_train =x_train.reshape(x_train.shape[0],x_train.shape[1] , 1)
+    x_test = x_test.reshape(x_test.shape[0],x_test.shape[1] , 1)
+    print(x_train.shape)
+    print(y_train.shape)
+    print(x_test.shape)
+
+    return x_train, y_train, x_test, y_test
+
+
+####### Nous allons regarder ici si le time step inlfue sur la qualité de la prédiction
+####### Nous allons tester des valeurs de time_step pour 100,150 et 200
+####### Test à ajouter : 3 modèles de préductions de stock return (mise en valeur de la variation non le prix)
+
+### Ici Model 1
+print('model 1')
+time_step1 = 100
+x_train1, y_train1, x_test1, y_test1 = reshape_data(time_step1,train_data,test_data)
+model1, history1 = deep_network_LSTM('model1',x_train1,y_train1,x_test1,y_test1,epochs=100)
+plot_hp(history1,'loss')
+plot_hp(history1,'accuracy')
+prediction1 = prediction_model_plot(model1,x_train1,y_train1,x_test1,y_test1, values,look_back=time_step1)
+
+
+### Ici Model 2
+print('model 2')
+time_step2 = 150
+x_train2, y_train2, x_test2, y_test2 = reshape_data(time_step2,train_data,test_data)
+model2, history2 = deep_network_LSTM('model2',x_train2,y_train2,x_test2,y_test2,epochs=100)
+plot_hp(history2,'loss')
+plot_hp(history2,'accuracy')
+prediction2 = prediction_model_plot(model2,x_train2,y_train2,x_test2,y_test2, values,look_back=time_step2)
+
+
+### Ici Model 3
+print('model 3')
+time_step3 = 200
+x_train3, y_train3, x_test3, y_test3 = reshape_data(time_step3,train_data,test_data)
+model3, history3 = deep_network_LSTM('model3',x_train3,y_train3,x_test3,y_test3,epochs=100)
+plot_hp(history3,'loss')
+plot_hp(history3,'accuracy')
+prediction3 = prediction_model_plot(model3,x_train3,y_train3,x_test3,y_test3, values,look_back=time_step3)
 
 
 
