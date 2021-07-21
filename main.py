d2721bb43b02bee9c137ebcd66eebe4e5cebfb0f
Ortes <malo.allee@gmail.com>
2021-07-21 17:47:51 +0200

    Convert to relative values

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ plt.close("all")
 
 data = pd.read_csv("data.csv", header=None)
 print(data)
-values = data[4].to_numpy()
+originalValues = data[4].to_numpy()
+values = []
 for i in range(len(data) - 1):
-    values[i] = ((values[i] / values[i + 1]) - 1) * 10
-values = values[:len(values) - 1]
+    values.append(((originalValues[i] / originalValues[i + 1]) - 1) * 10)
+values = np.array(values)
+originalValues = originalValues[:len(originalValues) - 1]
 
 # ts = pd.Series(values[:, 1])
 # plt.figure()
@@ -31,14 +33,21 @@ training_dataset_length = len(train_data)
 # Used for visualization and test purposes
 all_mid_data = np.concatenate([train_data, test_data], axis=0)
 
+prediction_foresee = 40
+
 
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, time_step):
     dataX, dataY = [], []
-    for i in range(len(dataset) - time_step - 1):
-        a = dataset[i:(i + time_step)]
+    for i in range(len(dataset) - 1 - time_step - prediction_foresee):
+        a = dataset[i:i + time_step]
         dataX.append(a)
-        dataY.append(dataset[i + time_step])
+        # Get max of the 40 next values
+        max_price = originalValues[i + time_step: i + time_step + prediction_foresee].max()
+        current_price = originalValues[i]
+        prediction = 1 if max_price > current_price else 0
+        prediction = .5 if current_price * 1.005 > max_price > current_price * 0.995 else prediction
+        dataY.append(prediction)
     return np.array(dataX), np.array(dataY)
 
 
@@ -87,9 +96,6 @@ def prediction_model_plot(model, x_train, y_train, x_test, y_test, values, look_
     train_predict = model.predict(x_train)
     test_predict = model.predict(x_test)
     ##Transformback to original form
-    train_predict = scaler.inverse_transform(train_predict)
-    test_predict = scaler.inverse_transform(test_predict)
-
     math.sqrt(mean_squared_error(y_train, train_predict))
     ### Test Data RMSE
     math.sqrt(mean_squared_error(y_test, test_predict))
@@ -102,13 +108,26 @@ def prediction_model_plot(model, x_train, y_train, x_test, y_test, values, look_
     trainPredictPlot[look_back:len(train_predict) + look_back, :] = train_predict
     # shift test predictions for plotting
     testPredictPlot = np.empty_like(values)
-    testPredictPlot[:, :] = np.nan
-    testPredictPlot[len(train_predict) + (look_back * 2) + 1:len(values) - 1, :] = test_predict
+    testPredictPlot[:] = np.nan
+    testPredictPlot[len(train_predict) + (look_back * 2) + 1 + 80:len(values) - 1] = test_predict
     # plot baseline and predictions
-    plt.plot(scaler.inverse_transform(values))
-    plt.plot(trainPredictPlot)
-    plt.plot(testPredictPlot)
-    plt.axis([x_min, x_max, y_min, y_max])  # permet de zoomer sur une partie de la courbe
+    fig = plt.figure(figsize=(12, 1), dpi=1200)
+
+    axe1 = fig.add_subplot(111)
+    axe1.plot(x, values, linewidth=0.033333)
+    axe1.set_ylabel('values')
+
+    axe2 = axe1.twinx()
+    axe2.set_ylabel('trainPredictPlot')
+    axe2.hist(trainPredictPlot, 100, edgecolor="k",color='yellow')
+
+
+    axe3 = axe1.twinx()
+    axe3.set_ylabel('testPredictPlot')
+    axe3.hist(testPredictPlot, 100, edgecolor="k", color='green')
+    # plt.plot(trainPredictPlot, linewidth=0.033333)
+    # plt.plot(testPredictPlot, linewidth=0.033333)
+    # plt.axis([x_min, x_max, y_min, y_max])  # permet de zoomer sur une partie de la courbe
     plt.show()
 
 
@@ -116,7 +135,7 @@ def plot_hp(model_plot, hyperparameter):
     train_hp = hyperparameter
     validation_hp = 'val_' + hyperparameter
     model = model_plot
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(12, 6), dpi=80)
     plt.plot(model.history[train_hp], label='train')
     plt.plot(model.history[validation_hp], alpha=0.7, label='validation')
     plt.xlabel(hyperparameter)
@@ -148,27 +167,27 @@ def reshape_data(time_step, train_data, test_data):
 
 ### Ici Model 1
 print('model 1')
-time_step1 = 100
+time_step1 = 200
 x_train1, y_train1, x_test1, y_test1 = reshape_data(time_step1, train_data, test_data)
-model1, history1 = deep_network_LSTM('model1', x_train1, y_train1, x_test1, y_test1, time_step1, epochs=2)
+model1, history1 = deep_network_LSTM('model1', x_train1, y_train1, x_test1, y_test1, time_step1, epochs=1)
 plot_hp(history1, 'loss')
 plot_hp(history1, 'accuracy')
 prediction1 = prediction_model_plot(model1, x_train1, y_train1, x_test1, y_test1, values, look_back=time_step1)
 
-### Ici Model 2
-print('model 2')
-time_step2 = 100
-x_train2, y_train2, x_test2, y_test2 = reshape_data(time_step2, train_data, test_data)
-model2, history2 = deep_network_LSTM('model2', x_train2, y_train2, x_test2, y_test2, time_step2, epochs=2)
-plot_hp(history2, 'loss')
-plot_hp(history2, 'accuracy')
-prediction2 = prediction_model_plot(model2, x_train2, y_train2, x_test2, y_test2, values, look_back=time_step2)
-
-### Ici Model 3
-print('model 3')
-time_step3 = 100
-x_train3, y_train3, x_test3, y_test3 = reshape_data(time_step3, train_data, test_data)
-model3, history3 = deep_network_LSTM('model3', x_train3, y_train3, x_test3, y_test3, time_step3, epochs=2)
-plot_hp(history3, 'loss')
-plot_hp(history3, 'accuracy')
-prediction3 = prediction_model_plot(model3, x_train3, y_train3, x_test3, y_test3, values, look_back=time_step3)
+# ### Ici Model 2
+# print('model 2')
+# time_step2 = 100
+# x_train2, y_train2, x_test2, y_test2 = reshape_data(time_step2, train_data, test_data)
+# model2, history2 = deep_network_LSTM('model2', x_train2, y_train2, x_test2, y_test2, time_step2, epochs=4)
+# plot_hp(history2, 'loss')
+# plot_hp(history2, 'accuracy')
+# prediction2 = prediction_model_plot(model2, x_train2, y_train2, x_test2, y_test2, values, look_back=time_step2)
+#
+# ### Ici Model 3
+# print('model 3')
+# time_step3 = 100
+# x_train3, y_train3, x_test3, y_test3 = reshape_data(time_step3, train_data, test_data)
+# model3, history3 = deep_network_LSTM('model3', x_train3, y_train3, x_test3, y_test3, time_step3, epochs=4)
+# plot_hp(history3, 'loss')
+# plot_hp(history3, 'accuracy')
+# prediction3 = prediction_model_plot(model3, x_train3, y_train3, x_test3, y_test3, values, look_back=time_step3)
