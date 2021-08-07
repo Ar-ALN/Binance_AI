53f4fbdb3de1cc0f12566e55f1957e9c5fc81e0d
asran974 <63764915+asran974@users.noreply.github.com>
2021-08-07 23:03:54 +0200

    Reshape fixed price input fixed

--- main.py
+++ main.py
@@ -215,9 +215,9 @@ def train_dddqn(env):
     memory_size = 200
     batch_size = 50
     epsilon = 1.0
-    epsilon_decrease = 1e-4
+    epsilon_decrease = 1e-3
     epsilon_min = 0.1
-    start_reduce_epsilon = 100
+    start_reduce_epsilon = 200
     train_freq = 10
     update_q_freq = 20
     gamma = 0.97
